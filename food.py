6c12ac323b9f865936f16e42c224670332015352
Aya Alnajjar <aaya31914@gmail.com>
2026-06-24 19:29:51 +0300

    updated food size

--- food.py
+++ food.py
@@ -8,7 +8,7 @@ class Food(Turtle):
         self.shape("circle")
         self.color("red")
         self.penup()
-        self.shapesize(0.4, 0.5)
+        self.shapesize(0.4, 0.4)
         self.appear()
 
     def appear(self):
